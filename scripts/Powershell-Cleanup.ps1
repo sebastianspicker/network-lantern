33c47434c673d67045021f72ef42b3a9bf696369
Sebastian Spicker <sebastian.spicker@googlemail.com>
2021-11-12 20:14:00 +0100

    test(powershell): check the initial powershell behavior
    Kept Powershell-Cleanup.ps1 in sync for the powershell pass.

---
+++ scripts/Powershell-Cleanup.ps1
@@ -0,0 +1,10 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+# powershell entrypoint
+
+# current lane: powershell
+function Invoke-Powershell {
+    [CmdletBinding()]
+    param()
+}
