b2d12a85de6e9131d72561ab2f54f098a889b3d9
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-04-18 23:59:00 +0200

    test(throughput): add a basic smoke pass for throughput

---
+++ scripts/Throughput-Followup.ps1
@@ -0,0 +1,10 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+# throughput entrypoint
+
+# current lane: throughput
+function Invoke-Throughput {
+    [CmdletBinding()]
+    param()
+}
